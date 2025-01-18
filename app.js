4f4f55ac64024a64f56acf332f0b5dc20786d59d
Tushar Patil <tp909580@gmail.com>
2025-01-18 20:29:14 +0530

    Improved responsivness

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
+const express = require("express");
 const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
